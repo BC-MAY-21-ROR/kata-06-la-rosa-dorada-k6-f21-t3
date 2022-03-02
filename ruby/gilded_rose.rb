1b1bea85dfbd579f48bddc98d5b9a1c926ecf766
Alejandro Curiel <alex_curielg@hotmail.com>
2022-03-02 16:23:17 -0600

    Add corrections rubocop
    Co-authored-by: Javi1499 <Javi1499@users.noreply.github.com>
    Co-authored-by: brocha05 <brocha_05@hotmail.com>

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,67 +1,62 @@
+# frozen_string_literal: true
+
+# Main class where all cases are evaluated
 class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      case item.name 
-      when '+5 Dexterity Vest'
-       # is_quality_negative?(item)
-        double_speed(item) #Pasa 1 dia
-      when 'Aged Brie'
-        cheese?(item)
-      when 'Elixir of the Mongoose'
-        #is_quality_negative?(item)
-        double_speed(item) #Pasa 1 dia
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        backstage?(item)
-      when 'Conjured Mana Cake'
-        cake?(item)
-      end
-      item.sell_in -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
+      product_filter(item)
+      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
     end
-    
   end
 
-  # def is_quality_negative?(item)
-  #   if(!item.quality.negative?)
-  #     item.quality -= 1
-  #   end
-  # end
-   
+  def product_filter(item)
+    case item.name
+    when '+5 Dexterity Vest', 'Elixir of the Mongoose'
+      double_speed(item) # Pasa 1 dia
+    when 'Aged Brie'
+      cheese?(item)
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      backstage?(item)
+    when 'Conjured Mana Cake'
+      cake?(item)
+    end
+  end
+
   def cheese?(item)
-    if item.quality < 50 
-      item.quality += (item.sell_in <= 0) ? 2 : 1
-      item.quality = 50 if item.quality > 50
-    end 
+    return unless item.quality < 50
+    item.quality += item.sell_in <= 0 ? 2 : 1
+    item.quality = 50 if item.quality > 50
   end
 
-  def backstage?(item) 
+  def backstage?(item)
     if item.sell_in <= 0
       item.quality = 0
     elsif item.sell_in <= 5
       item.quality += 3
     elsif item.sell_in <= 10
       item.quality += 2
-    else 
+    else
       item.quality += 1
     end
     item.quality = 50 if item.quality > 50
   end
 
   def double_speed(item)
-    item.quality -= (item.sell_in <= 0) ? 2 : 1
-    item.quality = 0 if item.quality < 0
-  end
-    
-  def cake?(item)
-    item.quality -= (item.sell_in < 0) ? 4 : 2
-    item.quality = 0 if item.quality < 0
+    item.quality -= item.sell_in <= 0 ? 2 : 1
+    item.quality = 0 if item.quality.negative?
   end
 
+  def cake?(item)
+    item.quality -= item.sell_in.negative? ? 4 : 2
+    item.quality = 0 if item.quality.negative?
+  end
 end
 
+# Class Item
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -71,8 +66,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
-  
 end
